f3dc0f4e51ef2548a9569c33886850991d915984
Jon Bloomer <jon@freaksauce.com>
2015-09-08 23:25:59 +1000

    adding some form elems ready to populate

--- client/Home.jsx
+++ client/Home.jsx
@@ -7,6 +7,10 @@ Home = React.createClass({
     }
   },
 
+  getInitialState: function() {
+    return {validated: false};
+  },
+
   handleSubmit() {
     console.log('handleSubmit');
     // console.log(this.refs.userStr.getDOMNode().value);
@@ -14,28 +18,40 @@ Home = React.createClass({
       this.refs.userStr.getDOMNode().value
     );
   },
-//
+
   validateUser(rnd) {
     console.log('rnd: '+rnd);
     var userData = Users.findOne({'rnd':parseInt(rnd)});
     var userId = userData._id;
     console.log(userData._id);
     Users.update({_id: userId}, {$set: {validated: 1} });
+    this.setState({validated: true});
   },
 
   render() {
+    var formClass = this.state.validated ? '' : 'hidden';
+
     return (
       <div>
-        <h2>Home</h2>
+        <h2 className="ui header">Home</h2>
         <p><a href="/dashboard">Dashboard</a></p>
 
-        <div className="ui form">
+        <div className="ui form segment">
           <div className="field">
             <label>User Random String</label>
             <input ref="userStr" type="text"/>
           </div>
           <div>
             <button className="ui primary submit button" onClick={this.handleSubmit}>Save</button>
+          </div>
+        </div>
+
+        <div className={formClass}>
+          <div className="ui segment form">
+            <h2 className="ui center aligned icon header">
+              <i className="circular users icon"></i>
+            </h2>
+            <h3 className="ui header">hidden form</h3>
           </div>
         </div>
 
--- client/Layout.jsx
+++ client/Layout.jsx
@@ -2,7 +2,7 @@ Layout = React.createClass({
   render() {
     return (
       <div className="ui container">
-        <header><h1>User Verification App</h1></header>
+        <header><h1 className="ui header">User Verification App</h1></header>
         <main>
           {this.props.content}
         </main>
