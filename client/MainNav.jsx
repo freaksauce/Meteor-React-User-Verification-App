8aada7f63e5dff2488d2b5605e86a03d06a0cf8e
Jon Bloomer <jon@freaksauce.com>
2015-09-14 21:28:47 +1000

    added reactive var to show dashboard link if logged in

--- client/MainNav.jsx
+++ client/MainNav.jsx
@@ -1,4 +1,22 @@
 MainNav = React.createClass({
+
+  mixins: [ReactMeteorData],
+
+  getMeteorData() {
+    return {
+      currentUser: Meteor.user()
+    }
+  },
+
+  getDashboardClasses() {
+    console.log(this.data.currentUser);
+    if (this.data.currentUser) {
+      return 'item';
+    }else{
+      return 'item hidden';
+    }
+  },
+
   render() {
     return (
       <div className="ui fixed inverted menu">
@@ -8,6 +26,7 @@ MainNav = React.createClass({
           </div>
           <a href="/" className="item">Home</a>
           <a href="/login" className="item">Login</a>
+          <a href="/dashboard" className={this.getDashboardClasses()}>Dashboard</a>
         </div>
       </div>
     );
